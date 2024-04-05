54c79551c8b353407c31f98c93b02e8dd5379805
Patrick Flood <Flood.49@gmail.com>
2024-04-05 14:59:18 -0400

    update

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -20,7 +20,9 @@ CREATE TABLE users (
     username varchar(50) NOT NULL,
     password_hash varchar(200) NOT NULL,
     salt varchar(200) NOT NULL,
-    user_type BIT NOT NULL,  --1 indicates employee; 0 is customer.
+    employee BIT NOT NULL,  --0 indicates employee; 1 is customer.
+	email varchar(75) UNIQUE NOT NULL,
+	active BIT NOT NULL, --0 indicates inactive, 1 is active (instead of deleting acct, set as inactive)
     CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
@@ -33,14 +35,35 @@ CREATE TABLE employee (
     CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) -- Add REFERENCES clause for foreign key
 );
 
+CREATE TABLE customer (
+    customer_id int IDENTITY(1001,1) NOT NULL,
+    username varchar(50) NOT NULL,
+    user_id INT NOT NULL,
+	contractor BIT NOT NULL, --0 indicates not contractor; 1 is contractor.
+	address varchar(125) NOT NULL,
+    CONSTRAINT PK_customer_id PRIMARY KEY (customer_id),
+    CONSTRAINT FK_user_id_ FOREIGN KEY (user_id) REFERENCES users(user_id) -- Add REFERENCES clause for foreign key
+);
+
+CREATE TABLE permit (
+    permit_id int IDENTITY(2001,1) NOT NULL, 
+	active BIT NOT NULL, --0 indicates inactive, 1 is active (instead of deleting acct, set as inactive)
+	customer_id int NOT NULL,
+	permit_address varchar(125) NOT NULL,
+    permit_type varchar(50) NOT NULL,
+    commercial BIT NOT NULL, --0 indicates residential; 1 is commercial.
+    CONSTRAINT PK_permit_id PRIMARY KEY (permit_id),
+    CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id) -- Add REFERENCES clause for foreign key
+);
+
 
 
 -- populate default data
 -- password for these is "password"
-INSERT INTO users (username, password_hash, salt, user_type) VALUES ('John Kevin Patrick','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=', 0);
-INSERT INTO users (username, password_hash, salt, user_type) VALUES ('Dio Moe Jeff','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=', 1);
+INSERT INTO users (username, password_hash, salt, employee, email, active) VALUES ('John Kevin Patrick','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=', 0, 'testdata@te.com',1);
+INSERT INTO users (username, password_hash, salt, employee, email, active) VALUES ('Dio Moe Jeff','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=', 1, 'employeeemail@company.com', 1);
 
-INSERT INTO employee (username, user_id, employee_type) VALUES ('Dio Moe Jeff', 2, 'inspector');
+--INSERT INTO employee (username, user_id, employee_type) VALUES ('Dio Moe Jeff', 2, 'inspector');
 
 --INSERT INTO employee (username, user_id, employee_type) VALUES ('user', 1, 'inspector')
 GO
