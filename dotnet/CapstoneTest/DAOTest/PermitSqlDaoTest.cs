879bc1ebd37716d3d30cc5a44661e76f308c8bdf
Patrick Flood <Flood.49@gmail.com>
2024-04-09 15:49:08 -0400

    update with integration tests

---
+++ dotnet/CapstoneTest/DAOTest/PermitSqlDaoTest.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Capstone.DAO;
+using Capstone.Models;
+
+namespace Tutorial.Tests.DAO
+{
+    [TestClass]
+    public class PermitSqlDaoTest : BaseDaoTests
+    {
+
+        private PermitSqlDao permitSqlDao;
+
+        [TestInitialize]
+        public override void Setup()
+        {
+            // Arrange - new instance of UserSqlDao before each and every test
+            permitSqlDao = new PermitSqlDao(ConnectionString);
+            base.Setup();
+
+            Permit permit = new Permit
+            {
+                Active = true,
+                CustomerId = 1001,
+                PermitAddress = "49 N Merkle st",
+                PermitType = "Residential",
+                Commercial = false,
+                PermitStatus = "Approved"
+            };
+            permitSqlDao.CreatePermit(permit);
+        }
+    
+
+        [TestMethod]
+        public void CreatePermit_ShouldCreatePermit()
+        {
+            // Arrange
+            Permit permit = new Permit
+            {
+                Active = true,
+                CustomerId = 1002,
+                PermitAddress = "123 Sesame St",
+                PermitType = "Residential",
+                Commercial = false,
+                PermitStatus = "Pending"
+
+            };
+
+            // Act
+            Permit createdPermit = permitSqlDao.CreatePermit(permit);
+
+            // Assert
+            Assert.IsNotNull(createdPermit);
+            Assert.AreEqual(permit.Active, createdPermit.Active);
+            Assert.AreEqual(permit.CustomerId, createdPermit.CustomerId);
+            Assert.AreEqual(permit.PermitAddress, createdPermit.PermitAddress);
+            Assert.AreEqual(permit.PermitType, createdPermit.PermitType);
+            Assert.AreEqual(permit.Commercial, createdPermit.Commercial);
+            Assert.AreEqual(permit.PermitStatus, createdPermit.PermitStatus);
+            Assert.IsTrue(createdPermit.PermitId > 0);
+        }
+
+        [TestMethod]
+        public void GetPermitById_ShouldReturnPermit()
+        {
+            // Arrange
+            Permit permit = new Permit
+            {
+                Active = true,
+                CustomerId = 1002,
+                PermitAddress = "123 Sesame St",
+                PermitType = "Residential",
+                Commercial = false,
+                PermitStatus = "Approved",
+            };
+
+            Permit newPermit = permitSqlDao.CreatePermit(permit);
+
+            // Act
+            Permit retrievedPermit = permitSqlDao.GetPermitById(newPermit.PermitId);
+
+            // Assert
+            Assert.IsNotNull(retrievedPermit);
+            Assert.AreEqual(newPermit.PermitId, retrievedPermit.PermitId);
+            Assert.AreEqual(newPermit.Active, retrievedPermit.Active);
+            Assert.AreEqual(newPermit.CustomerId, retrievedPermit.CustomerId);
+            Assert.AreEqual(newPermit.PermitAddress, retrievedPermit.PermitAddress);
+            Assert.AreEqual(newPermit.PermitType, retrievedPermit.PermitType);
+            Assert.AreEqual(newPermit.Commercial, retrievedPermit.Commercial);
+            Assert.AreEqual(newPermit.PermitStatus, retrievedPermit.PermitStatus);
+        }
+
+        [TestMethod]
+        public void ListPermits_ShouldReturnAllPermits()
+        {
+            // Arrange
+            Permit permit1 = new Permit
+            {
+                Active = true,
+                CustomerId = 1002,
+                PermitAddress = "123 Sesame St",
+                PermitType = "Residential",
+                Commercial = false,
+                PermitStatus = "Approved"
+            };
+
+            Permit permit2 = new Permit
+            {
+                Active = true,
+                CustomerId = 1003,
+                PermitAddress = "1063 Oak st",
+                PermitType = "Commercial",
+                Commercial = true,
+                PermitStatus = "Pending"
+            };
+
+            permitSqlDao.CreatePermit(permit1);
+            permitSqlDao.CreatePermit(permit2);
+
+            // Act
+            List<Permit> permits = permitSqlDao.ListPermits();
+
+            // Assert
+            Assert.IsNotNull(permits);
+            Assert.AreEqual(2, permits.Count);
+
+        }
+    }
+}
--- dotnet/CapstoneTest/DAOTest/UserSqlDaoTest.cs
+++ dotnet/CapstoneTest/DAOTest/UserSqlDaoTest.cs
@@ -20,18 +20,104 @@ namespace Tutorial.Tests.DAO
         }
 
         [TestMethod]
-        public void GetUsers()
+        public void GetUsers_ShouldReturnAllUsers()
         {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
 
-            // Arrange  - see above
+            // Act
+            IList<User> users = dao.GetUsers();
 
-
-            // Act - retrieve users
-            IList<User> users = userSqlDao.GetUsers();
-
-            // Assert - retrieved list not null and matches expected count
+            // Assert
             Assert.IsNotNull(users);
             Assert.AreEqual(2, users.Count);
+        }
+
+        [TestMethod]
+        public void GetUserById_ShouldReturnUser()
+        {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
+            int userId = 1001;
+
+            // Act
+            User user = dao.GetUserById(userId);
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.AreEqual(userId, user.UserId);
+        }
+
+        [TestMethod]
+        public void GetUserByUsername_ShouldReturnUser()
+        {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
+            string username = "testuser";
+
+            // Act
+            User user = dao.GetUserByUsername(username);
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.AreEqual(username, user.Username);
+        }
+
+        [TestMethod]
+        public void CreateUser_ShouldCreateUser()
+        {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
+            string username = "testuser";
+            string password = "password";
+            string role = "user";
+            bool isEmployee = false;
+            string email = "testuser@gmail.com";
+            bool active = true;
+
+            // Act
+            User newUser = dao.CreateUser(username, password, role, isEmployee, email, active);
+
+            // Assert
+            Assert.IsNotNull(newUser);
+            Assert.AreEqual(username, newUser.Username);
+            Assert.AreEqual(role, newUser.Role);
+            Assert.AreEqual(email, newUser.Email);
+            Assert.AreEqual(active, newUser.Active);
+        }
+
+        [TestMethod]
+        public void GetCustomerById_ShouldReturnCustomer()
+        {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
+            int customerId = 1001;
+
+            // Act
+            Customer customer = dao.GetCustomerById(customerId);
+
+            // Assert
+            Assert.IsNotNull(customer);
+            Assert.AreEqual(customerId, customer.CustomerId);
+        }
+
+        [TestMethod]
+        public void CreateCustomer_ShouldCreateCustomer()
+        {
+            // Arrange
+            UserSqlDao dao = new UserSqlDao(ConnectionString);
+            int userId = 1003;
+            bool isContractor = true;
+            string address = "1063 Oak St Columbus, OH";
+
+            // Act
+            Customer newCustomer = dao.CreateCustomer(userId, isContractor, address);
+
+            // Assert
+            Assert.IsNotNull(newCustomer);
+            Assert.AreEqual(userId, newCustomer.UserId);
+            //Assert.AreEqual(isContractor, newCustomer.IsContractor);
+            Assert.AreEqual(address, newCustomer.Address);
         }
 
     }
